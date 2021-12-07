f14e41ae05aec957ec6d2115e4288b06014dbba9
samuelokediji <samuelolaideokediji@gmail.com>
2021-12-07 01:05:36 +0100

    backend now microservices

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=cercuit
+export POSTGRES_PASSWORD=1stbillion
+export POSTGRES_HOST=cdr.cipky0gxx865.eu-west-3.rds.amazonaws.com
+export POSTGRES_DB=cercuit
+export AWS_BUCKET=sam-530680288131-dev
+export AWS_REGION=eu-west-3a
+export AWS_PROFILE=test
+export JWT_SECRET=testing
+export URL=http://localhost:8100
